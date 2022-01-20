e39e9f01d2174dd6bf8e6a5244ee50cdb3ce90d4
ericmonne <ericmfo@hotmail.com>
2022-01-20 15:17:51 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Olá, enfermeira! " + (a+b));*/
+    }
+}
